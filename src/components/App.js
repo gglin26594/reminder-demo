1c6f30257ab0e9628983bffa04cae5ed6815cb07
gglin26594 <lin.cityu@gmail.com>
2018-09-03 20:15:57 +1200

    init the layout of demo

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="title">Reminder Demo</div>
 
+        <div className="form-inline">
+          <div className="form-group">
+            <input type="text" className="form-control mr-3" placeholder="Remind me to..."/>
+          </div>
+          <button className="btn btn-primary">
+            Add Reminder
+          </button>
+        </div>
       </div>
     );
   }
